2be8e65eb94e373100d08a6cf7a8ac9d51680918
TizzyBac1994 <83019389+TizzyBac1994@users.noreply.github.com>
2021-04-28 21:24:12 +0100

    ClassDate
    6.1概述 构造方法
     public class Date
    
    extends Object
    implements Serializable, Cloneable, Comparable<Date>
    
    Date()
    Date(long date)
    
    6.2常用方法
    public long getTime()
    public void setTime(long time)

---
+++ DateDemo.java
@@ -0,0 +1,19 @@
+package Date;
+
+import java.util.Date;
+
+public class DateDemo {
+    public static void main(String[] args) {
+        Date d1 = new Date();
+        System.out.println(d1);
+
+        long date = 1000*60*60;
+        Date d2 = new Date(date);
+        System.out.println(d2);
+
+        //create obj
+        Date d3 = new Date();
+        System.out.println(d3.getTime()/1.0/1000/60/60/24/365);
+
+    }
+}
